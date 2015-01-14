328029318cd47ea654717c8899f04837a3f9ebbb
zahich <napoitel@abv.bg>
2015-01-14 21:03:38 +0200

    nu pagadi izrigni

--- js/transform.js
+++ js/transform.js
@@ -4,7 +4,8 @@
     $('.breaking_news a').hover(function(){
     var path = $(this).find('img').attr("src");
     $('.neimar').attr("src",path); 
-     
+    var figcap = $(this).find('figcaption').html();
+    $('.figcap1').html(figcap);
     }
 )});
 
