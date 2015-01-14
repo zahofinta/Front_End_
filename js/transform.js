a60ec8aba246e267047d340d0c42ed86f32b45d9
zahich <napoitel@abv.bg>
2015-01-14 20:27:42 +0200

    kini meso

--- js/transform.js
+++ js/transform.js
@@ -1,85 +1,11 @@
 
 	$(document).ready(function() {
    
-    $('.img1').hover(function(){
-    $('.logo').replaceWith('<img src="http://img2.sportal.bg/uploads/news/2014_52/p1/00524805.jpg" class="b1" >');
-    $('.b1').css({left:350,position:'relative'});
-
-    $('.b2').replaceWith('<img src="http://img2.sportal.bg/uploads/news/2014_52/p1/00524805.jpg" class="b1" >');
-    $('.b1').css({left:350,position:'relative'});
-
-    $('.b3').replaceWith('<img src="http://img2.sportal.bg/uploads/news/2014_52/p1/00524805.jpg" class="b1" >');
-    $('.b1').css({left:350,position:'relative'});
-    
-   $('.b4').replaceWith('<img src="http://img2.sportal.bg/uploads/news/2014_52/p1/00524805.jpg" class="b1" >');
-    $('.b1').css({left:350,position:'relative'});
+    $('.breaking_news a').hover(function(){
+    var path = $(this).find('img').attr("src");
+    $('.neimar').attr("src",path); 
      
-    },function(){
-
-     $('.b1').replaceWith('<img class="logo" src="logo/logo.jpg">');
-
-    });
-
-
-    $('.img2').hover(function(){
-      
-    $('.logo').replaceWith('<img src="http://img2.sportal.bg/uploads/news/2014_52/p1/00524760.jpg" class="b2" >');
-    $('.b2').css({left:350,position:'relative'});
-
-    $('.b1').replaceWith('<img src="http://img2.sportal.bg/uploads/news/2014_52/p1/00524760.jpg" class="b2" >');
-    $('.b2').css({left:350,position:'relative'});
-
-    $('.b3').replaceWith('<img src="http://img2.sportal.bg/uploads/news/2014_52/p1/00524760.jpg" class="b2" >');
-    $('.b2').css({left:350,position:'relative'});
-    
-    $('.b4').replaceWith('<img src="http://img2.sportal.bg/uploads/news/2014_52/p1/00524760.jpg" class="b2" >');
-    $('.b2').css({left:350,position:'relative'});
-    
-    
-    },function(){
-
-         $('.b2').replaceWith('<img class="logo" src="logo/logo.jpg">');
-    });
-
-    $('.img3').hover(function(){
-    
-    $('.logo').replaceWith('<img src="http://donbalon.eu/content/pics/578000.jpg "class="b3" >');
-    $('.b3').css({left:350,position:'relative',width:600});
-
-    $('.b2').replaceWith('<img src="http://donbalon.eu/content/pics/578000.jpg" class="b3" >');
-    $('.b3').css({left:350,position:'relative',width:600});
-
-    $('.b1').replaceWith('<img src="http://donbalon.eu/content/pics/578000.jpg" class="b3" >');
-    $('.b3').css({left:350,position:'relative',width:600});
-
-    $('.b4').replaceWith('<img src="http://donbalon.eu/content/pics/578000.jpg" class="b3" >');
-    $('.b3').css({left:350,position:'relative',width:600});
-    
-    
-    },function(){
-
-        $('.b3').replaceWith('<img class="logo" src="logo/logo.jpg">');
-
-    });
-
-    $('.img4').hover(function(){
-
-    $('.logo').replaceWith('<img src="http://www.donbalon.eu/content/pics/578653.jpg" class="b4" >');
-    $('.b4').css({left:350,position:'relative',width:600});
-
-    $('.b3').replaceWith('<img src="http://www.donbalon.eu/content/pics/578653.jpg" class="b4" >');
-    $('.b4').css({left:350,position:'relative',width:600});
-
-    $('.b2').replaceWith('<img src="http://www.donbalon.eu/content/pics/578653.jpg" class="b4" >');
-    $('.b4').css({left:350,position:'relative',width:600});
-
-    $('.b1').replaceWith('<img src="http://www.donbalon.eu/content/pics/578653.jpg" class="b4" >');
-    $('.b4').css({left:350,position:'relative',width:600});
-    
-    },function(){
-         $('.b4').replaceWith('<img class="logo" src="logo/logo.jpg">');
-    });
-
-});
+    }
+)});
 
 	
